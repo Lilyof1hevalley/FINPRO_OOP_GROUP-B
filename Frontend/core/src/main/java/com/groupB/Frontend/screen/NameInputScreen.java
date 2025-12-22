6431d6921f981d9a1eb929067ab7ab175110d7ac
naylapramesti <naylapramesti.adhina@gmail.com>
2025-12-22 23:09:36 +0700

    Update NameInputScreen.java

--- Frontend/core/src/main/java/com/groupB/Frontend/screen/NameInputScreen.java
+++ Frontend/core/src/main/java/com/groupB/Frontend/screen/NameInputScreen.java
@@ -12,7 +12,7 @@ public class NameInputScreen implements Screen {
     private SpaceShooterGame game;
     private SpriteBatch batch;
     private BitmapFont font;
-    private final int SCREEN_SIZE = 800; // Rasio 1:1
+    private final int SCREEN_SIZE = 800; 
 
     private Texture logoTexture;
     private Rectangle playButtonBounds;
@@ -26,26 +26,23 @@ public class NameInputScreen implements Screen {
         font = new BitmapFont();
         font.getData().setScale(1.5f);
 
-        // Memastikan file logo ada di folder assets utama
         logoTexture = new Texture(Gdx.files.internal("logo.png"));
 
-        // Matematika Tengah: (Layar/2) - (Lebar/2)
         float btnW = 200, btnH = 60;
-        float btnX = (SCREEN_SIZE / 2f) - (btnW / 2f); // Hasilnya 300
+        float btnX = (SCREEN_SIZE / 2f) - (btnW / 2f); 
         float btnY = 250;
         playButtonBounds = new Rectangle(btnX, btnY, btnW, btnH);
 
         Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
             public boolean touchDown(int x, int y, int p, int b) {
-                // Konversi Y karena LibGDX koordinat mouse terbalik
                 if (playButtonBounds.contains(x, SCREEN_SIZE - y)) isButtonPressed = true;
                 return true;
             }
             @Override
             public boolean touchUp(int x, int y, int p, int b) {
                 if (isButtonPressed && playButtonBounds.contains(x, SCREEN_SIZE - y)) {
-                    game.setPlayerName("Player"); // Nama default karena input diapus
+                    game.setPlayerName("Player"); 
                     game.setScreen(new GameScreen(game));
                 }
                 isButtonPressed = false;
@@ -56,17 +53,15 @@ public class NameInputScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1, 1, 1, 1); // Background Putih
+        Gdx.gl.glClearColor(1, 1, 1, 1); 
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
 
-        // LOGO: Digambar kotak (400x400) biar gak miring karena file aslinya kotak
         float logoSize = 400;
         float logoX = (SCREEN_SIZE / 2f) - (logoSize / 2f);
         batch.draw(logoTexture, logoX, 400, logoSize, logoSize);
 
-        // TOMBOL PLAY
         batch.setColor(isButtonPressed ? Color.MAROON : Color.PINK);
         batch.draw(Assets.whitePixel, playButtonBounds.x, playButtonBounds.y, playButtonBounds.width, playButtonBounds.height);
 
